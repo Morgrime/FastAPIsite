943e71c51969431c026f8acdcbb107b9fe531364
Morgrime <daniel2000@mail.ru>
2025-01-21 15:40:10 +0500

    Added router to App

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from uvicorn import run
 
+from endpoints.user_endpoints import router as user_router
+
 app = FastAPI()
+app.include_router(user_router)
 
 if __name__ == '__main__':
     run(app=app)
